7baafb3926712e24e88d2b7247a91afea33ced79
Drin Racaj <145534814+drinracaj@users.noreply.github.com>
2025-08-27 22:29:49 +0200

    feat: implement logging capabilities (#64)

--- src/core/src/main.rs
+++ src/core/src/main.rs
@@ -1,8 +1,24 @@
-use miel::*;
 use controller_handler::*;
+use error_handling::*;
+use miel::{types::ConfigError, *};
+
+use log::{debug, error, info, trace, warn};
 
 fn main() {
     let _controller = Controller::new();
+
+    // Example how to log
+    // https://docs.rs/env_logger/latest/env_logger/
+    env_logger::init();
+
+    //let e = ConfigError::InvalidFormat;
+    // info!("informing {:?}", e); -> will only work if the enum implement Debug (add #[derive(Debug) to the enum]
+    debug!("debugging");
+    warn!("warning");
+    error!("error");
+    trace!("trace");
+
+    // run with RUST_LOG=[target]=[level] cargo run, ex: RUST_LOG=miel cargo run -> displays every type of log
 }
 
 #[cfg(test)]
