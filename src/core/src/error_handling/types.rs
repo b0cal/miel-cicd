f7dcba9943e9d8718044c096f3902818881a1863
Drin Racaj <145534814+drinracaj@users.noreply.github.com>
2025-08-29 15:19:56 +0200

    feat: add module specfic error enums (#72)
    * feat: add module specfic error enums
    
    * doc: uml class diagram (#73)
    
    * feat: add uml class diagram
    
    * feat: simplify class diagram
    
    ---------
    
    Co-authored-by: Aubry Mangold <31241846+a-mango@users.noreply.github.com>
    
    * doc: update the class diagram
    
    ---------
    
    Co-authored-by: Aubry Mangold <31241846+a-mango@users.noreply.github.com>

--- src/core/src/error_handling/types.rs
+++ src/core/src/error_handling/types.rs
@@ -1,8 +1,15 @@
 #[derive(Debug)]
 pub enum ControllerError {
-    //ex : Config(ConfigError),
+    Config(ConfigError),
+    Network(NetworkError),
+    Session(SessionError),
+    Container(ContainerError),
+    Storage(StorageError),
+    Web(WebError),
 }
 
+// InvalidFormat and MissingField are not currently used in Config because the crate toml does
+// only differentiate between the two in the message returned with the Error, so better to use TomlError(String)
 #[derive(Debug)]
 pub enum ConfigError {
     InvalidFormat,
@@ -17,16 +24,43 @@ pub enum ConfigError {
 }
 
 #[derive(Debug)]
-pub enum SessionError {}
+pub enum SessionError {
+    CreationFailed,
+    ContainerError(ContainerError),
+    StorageError(StorageError),
+    CaptureError(CaptureError),
+}
 
 #[derive(Debug)]
-pub enum WebError {}
+pub enum WebError {
+    RequestFailed,
+    StartFailed(String),
+}
 
 #[derive(Debug)]
-pub enum NetworkError {}
+pub enum NetworkError {
+    ConnectionFailed,
+    ServiceDetectionFailed,
+    BindFail(std::io::Error),
+}
 
 #[derive(Debug)]
-pub enum ContainerError {}
+pub enum ContainerError {
+    RuntimeNotAvailable,
+    CreationFailed(String),
+    StartFailed,
+}
 
 #[derive(Debug)]
-pub enum StorageError {}
+pub enum StorageError {
+    ConnectionFailed,
+    WriteFailed,
+    ReadFailed,
+}
+
+#[derive(Debug)]
+pub enum CaptureError {
+    TcpStreamError(std::io::Error),
+    StdioError(std::io::Error),
+    StorageError(StorageError),
+}
